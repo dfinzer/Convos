c286de63a57381651ea09b9b638eaf01c77a4085
dfinzer <devin_finzer@brown.edu>
2012-08-27 13:50:40 -0700

    Remove logging statement.

--- static/js/convos.js
+++ static/js/convos.js
@@ -1,7 +1,6 @@
 function login() {
   $.post("/login", {}, function(response) {
     data = $.parseJSON(response);
-    console.log(data)
     if (data.status == "pending") {
       $("#verification-code").text(data.verification_code)
     }
