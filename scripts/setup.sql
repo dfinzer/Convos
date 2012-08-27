99c7de647b1a337a6870463541cc1a6e27ed7221
dfinzer <devin_finzer@brown.edu>
2012-08-26 22:33:26 -0700

    Support for inserting and retrieving users.

---
+++ scripts/setup.sql
@@ -0,0 +1,20 @@
+DROP USER 'devin'@'localhost';
+CREATE USER 'devin'@'localhost' IDENTIFIED BY 'convos';
+GRANT ALL PRIVILEGES ON *.* TO 'devin'@'localhost' WITH GRANT OPTION;
+CREATE DATABASE convos;
+
+CREATE TABLE  `convos`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR( 100 ),
+  `first_name` VARCHAR ( 100 ),
+  `last_name` VARCHAR ( 100 ),
+  `email` VARCHAR ( 100 ),
+  `locale` VARCHAR ( 100 ),
+  `username` VARCHAR( 100 ),
+  `gender` VARCHAR( 100 ),
+  `fb_uid` VARCHAR( 100 ),
+  `fb_verified` BOOLEAN,
+  `registration_status` VARCHAR( 100 ) NOT NULL, -- Can be pending or registered.
+  PRIMARY KEY (  `id` ),
+  UNIQUE KEY (`fb_uid`)
+) ENGINE = MYISAM;
