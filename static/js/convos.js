8d0a0eba6c8de87d64f962a91f24474b19edbe10
dfinzer <devin_finzer@brown.edu>
2012-08-26 23:33:06 -0700

    Remove logout stub for now.

--- static/js/convos.js
+++ static/js/convos.js
@@ -2,9 +2,8 @@ function login() {
   $.post("/login", {}, function(response) {
     data = $.parseJSON(response);
     console.log(data)
+    if (data.status == "pending") {
+      $("#verification-code").text(data.verification_code)
+    }
   });
-}
-
-function logout() {
-  // TODO: implement.
 }
