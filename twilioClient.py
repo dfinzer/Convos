a7b03bbeb811badc9d080028396dc4a48903e9a3
dfinzer <devin_finzer@brown.edu>
2012-08-28 20:03:06 -0700

    Consistent naming.

--- twilioClient.py
+++ twilioClient.py
@@ -1,14 +1,15 @@
 from twilio.rest import TwilioRestClient
 
 # Twilio configuration.
-account_sid = "AC52a3d465bd5577c994ebad881c1ac48a"
-auth_token = "5fdc4c5e212bd4e3301211631ad5e729"
-twilio_phone_number = "+19252720008"
-client = TwilioRestClient(account_sid, auth_token)
+TWILIO_ACCOUNT_SID = "AC52a3d465bd5577c994ebad881c1ac48a"
+TWILIO_AUTH_TOKEN = "5fdc4c5e212bd4e3301211631ad5e729"
+TWILIO_PHONE_NUMBER = "+19252720008"
+
+client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 class TwilioClient:
   def sendMessage(self, toNumber, body):
-    client.sms.messages.create(to=toNumber, from_=twilio_phone_number, body=body)
+    client.sms.messages.create(to=toNumber, from_=TWILIO_PHONE_NUMBER, body=body)
       
 class TwilioTestClient(TwilioClient):
   def sendMessage(self, toNumber, body):
