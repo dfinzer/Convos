8dd3f6d30a75f7617fb72af1d9c9edb429d6d9c6
dfinzer <devin_finzer@brown.edu>
2012-08-30 22:18:38 -0700

    Google analytics for prod.

--- static/js/convos.js
+++ static/js/convos.js
@@ -1,9 +1,21 @@
 var hasSetPollingInterval = false;
 var pollingIntervalId;
 
+// Google analytics.
+if (isProductionEnvironment()) {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-16848831-7']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+}
+
 $(document).ready(function() {
   // Log home page visit.
-  console.log("here")
   logVisitedPage("home");
 });
 
